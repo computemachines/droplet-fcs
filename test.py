1d62c9c8e8ba1a1e930367fbec48b5303ad50c7a
Tyler Parker <tparker@umass.edu>
2015-08-05 23:44:28 -0400

    Fixed empty histogram error

--- test.py
+++ test.py
@@ -1,12 +1,14 @@
 #! /usr/bin/python
 
 import matplotlib.pyplot as plt
-import pickle
+import pickle, pickletools
+import numpy as np
 import fcs
 
 out = fcs.fcs()
 
-plt.hist(out[0], bins=500)
+if len(out[0]) > 0:
+    plt.hist(out[0], bins=500)
 #plt.show()
 
 debug = pickle.loads(out[2])
